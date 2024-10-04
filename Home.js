9f84ebd30295e985690b860aff94d657208d568c
felipemal66 <166239267+felipemal66@users.noreply.github.com>
2024-10-04 15:48:20 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,1 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Bem-vindo!</h1>
+      <p>Escolha uma das opções abaixo:</p>
+      <div>
+        <button onClick={() => navigate("/posts")}>Ir para Posts</button>
+        <button onClick={() => navigate("/users")}>Ver Últimos Usuários</button>
+        <button onClick={() => navigate("/movies")}>
+          Ver Filmes Populares
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
